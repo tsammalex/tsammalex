2cdd3109fcf0cc379f6b500c667ae9b7968cab6c
xrotwang <xrotwang@googlemail.com>
2014-11-30 14:45:44 +0100

    started work on using travis for data integrity checks

---
+++ setup.py
@@ -0,0 +1,25 @@
+import os
+
+from setuptools import setup, find_packages
+
+
+setup(name='tsammalexdata',
+      version='0.0',
+      description='data for the tsammalex site',
+      long_description='',
+      classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        ],
+      author='',
+      author_email='',
+      url='',
+      keywords='data linguistics',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[],
+      tests_require=[],
+      test_suite="tsammalexdata",
+)
---
+++ tsammalexdata/tests/test_csv.py
@@ -0,0 +1,41 @@
+import os
+import csv
+
+import tsammalexdata
+
+
+SUCCESS = True
+
+
+def error(msg, name, line=''):
+    global SUCCESS
+    SUCCESS = False
+    if line:
+        line = ' line %s' % line
+    print('ERROR in %s%s: %s' % (name, line, msg))
+
+
+def data_file(name):
+    return os.path.join(os.path.dirname(tsammalexdata.__file__), 'data', name)
+
+
+def read_csv(name, unique=None):
+    uniquevalues = set()
+    rows = []
+    with open(data_file(name + '.csv')) as csvfile:
+        for line, row in enumerate(csv.DictReader(csvfile)):
+            line += 2
+            if unique:
+                if row[unique] in uniquevalues:
+                    error(name, 'non-unique id: %s' % row[unique], line)
+                uniquevalues.add(row[unique])
+            rows.append((line, row))
+    return rows
+
+
+def test():
+    species = {s[1]['id']: 1 for s in read_csv('species')}
+    for line, img in read_csv('images'):
+        if img['species_id'] not in species:
+            error('invalid species id referenced: %s' % img['species_id'], 'images', line)
+
